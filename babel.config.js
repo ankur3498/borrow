93aa9f87d8a7bcdc80e2752bca5855f9be645534
Himanshu Sharma <himanshushar213@gmail.com>
2025-12-10 13:08:09 +0530

    hbjh

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,3 @@
-// babel.config.js
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    // other plugins...
-    'react-native-reanimated/plugin', // must be last
-    // if your setup/docs request it: 'react-native-worklets/plugin'
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,25 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require("path");
+const { getDefaultConfig } = require("@react-native/metro-config");
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (() => {
+  const projectRoot = __dirname;
+  const config = getDefaultConfig(projectRoot);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  // Windows path normalization
+  config.projectRoot = path.resolve(projectRoot);
+  config.watchFolders = [path.resolve(projectRoot)];
+
+  // SVG transformer
+  config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+
+  config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...config.resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
